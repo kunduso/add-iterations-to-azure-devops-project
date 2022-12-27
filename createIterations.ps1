1dbed0906e5dc1953ad5f844730b0f2f3b926a91
Sourav Kundu <skundu.dev@gmail.com>
2022-12-27 11:25:51 -0600

    more changes, reorganizing variables

--- createIterations.ps1
+++ createIterations.ps1
@@ -1,10 +1,11 @@
 ﻿param(
+    [Parameter(Mandatory=$true)][String]$Organization,
     [Parameter(Mandatory=$true)][String]$PAT,
     [Parameter(Mandatory=$true)][String]$Project,
     [Parameter(Mandatory=$true)][String]$TeamName,
-    [Parameter(Mandatory=$true)][String]$Organization,
     [Parameter(Mandatory=$true)][String]$YearOfIteration,
-    [Parameter(Mandatory=$true)][String]$FirstDateOfIteration,
+    [Parameter(Mandatory=$true)][String]$MonthOfIteration,
+    [Parameter(Mandatory=$true)][String]$StartDateOfIteration,
     [Parameter(Mandatory=$true)][String]$NumberOfSprints
 )
 Write-Host "Organization: $Organization"
@@ -13,7 +14,7 @@ Write-Host "Team: $TeamName"
 
 #$NumberOfSprints = 4
 #//auto setting variables based on values provided
-$StartDate = Get-Date -Year $YearOfIteration -Month 1 -Day $FirstDateOfIteration
+$StartDate = Get-Date -Year $YearOfIteration -Month $MonthOfIteration -Day $StartDateOfIteration
 $RootPath = "\"+$Project+"\Iteration\"+$StartDate.Year
 $ParentIteration = "\"+$Project+"\Iteration"
 
@@ -37,7 +38,7 @@ else {
 $StartDateIteration = $StartDate
 For ($i=1; $i -le $NumberOfSprints; $i++) 
 {
-    $Sprint = '2023-iteration-' + $i
+    $Sprint = '$($StartDate.Year)-iteration-' + $i
     $FinishDateIteration = $StartDateIteration.AddDays(13)
     $createIteration = az boards iteration project create --name $Sprint --path $RootPath --start-date $StartDateIteration --finish-date $FinishDateIteration --org $Organization --project $Project | ConvertFrom-Json
     $addIteration = az boards iteration team add --id $createIteration.Identifier --team $TeamName --org $Organization --project $Project | ConvertFrom-Json
