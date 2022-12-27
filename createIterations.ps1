c0e0cb77992569e7169a35706abb882085c617e1
Sourav Kundu <skundu.dev@gmail.com>
2022-12-27 06:53:13 -0600

    reviewing the list command

--- createIterations.ps1
+++ createIterations.ps1
@@ -23,16 +23,17 @@ $ParentIteration = "\"+$Project+"\Iteration"
 Write-Output $PAT | az devops login --org $Organization
 Write-Host '===Configuring connection to organization and Team Project'
 az devops configure --defaults organization=$Organization project=$Project
-$CreateRootIteration = az boards iteration project create --name $StartDate.Year --path $ParentIteration | ConvertFrom-Json
-Write-Host 'Created Root path: '$CreateRootIteration.name
-    $StartDateIteration = $StartDate
-For ($i=1; $i -le $NumberOfSprints; $i++) 
-{
-    $Sprint = '2023-iteration-' + $i
-    $FinishDateIteration = $StartDateIteration.AddDays(13)
-    $createIteration = az boards iteration project create --name $Sprint --path $RootPath --start-date $StartDateIteration --finish-date $FinishDateIteration --org $Organization --project $Project | ConvertFrom-Json
-    $addIteration = az boards iteration team add --id $createIteration.Identifier --team $TeamName --org $Organization --project $Project | ConvertFrom-Json
-    Write-Host $addIteration.name 'created on path'$addIteration.path
-    $StartDateIteration = $FinishDateIteration.AddDays(1)
-}
+az boards iteration project list --depth 1
+#$CreateRootIteration = az boards iteration project create --name $StartDate.Year --path $ParentIteration | ConvertFrom-Json
+# Write-Host 'Created Root path: '$CreateRootIteration.name
+#     $StartDateIteration = $StartDate
+# For ($i=1; $i -le $NumberOfSprints; $i++) 
+# {
+#     $Sprint = '2023-iteration-' + $i
+#     $FinishDateIteration = $StartDateIteration.AddDays(13)
+#     $createIteration = az boards iteration project create --name $Sprint --path $RootPath --start-date $StartDateIteration --finish-date $FinishDateIteration --org $Organization --project $Project | ConvertFrom-Json
+#     $addIteration = az boards iteration team add --id $createIteration.Identifier --team $TeamName --org $Organization --project $Project | ConvertFrom-Json
+#     Write-Host $addIteration.name 'created on path'$addIteration.path
+#     $StartDateIteration = $FinishDateIteration.AddDays(1)
+# }
 az devops logout
