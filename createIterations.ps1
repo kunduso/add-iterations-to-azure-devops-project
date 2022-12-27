2e03ba8f9e1b9fedbed08c2101b9b7be1dfba594
Sourav Kundu <skundu.dev@gmail.com>
2022-12-27 11:15:03 -0600

    updated statements

--- createIterations.ps1
+++ createIterations.ps1
@@ -1,10 +1,4 @@
-﻿#//***Please update the values in below 5 lines before working with this code***
-# $PAT ="enter-PAT-value"
-# $Organization = "https://dev.azure.com/$(your-organization-name)/"
-# $Project="$(your-project-name)"
-# $TeamName = "$(your-team-name)"
-
-param(
+﻿param(
     [Parameter(Mandatory=$true)][String]$PAT,
     [Parameter(Mandatory=$true)][String]$Project,
     [Parameter(Mandatory=$true)][String]$TeamName,
@@ -12,10 +6,11 @@ param(
     [Parameter(Mandatory=$true)][String]$YearOfIteration,
     [Parameter(Mandatory=$true)][String]$FirstDateOfIteration
 )
-Write-Host "Value of Project: $Project"
-Write-Host "Value of Team: $TeamName"
-Write-Host "Value of Team: $Organization"
-$NumberOfSprints = 26
+Write-Host "Organization: $Organization"
+Write-Host "Project: $Project"
+Write-Host "Team: $TeamName"
+
+$NumberOfSprints = 4
 #//auto setting variables based on values provided
 $StartDate = Get-Date -Year $YearOfIteration -Month 1 -Day $FirstDateOfIteration
 $RootPath = "\"+$Project+"\Iteration\"+$StartDate.Year
@@ -23,10 +18,11 @@ $ParentIteration = "\"+$Project+"\Iteration"
 
 #//execution begins
 Write-Output $PAT | az devops login --org $Organization
-Write-Host '===Configuring connection to organization and Team Project'
+Write-Host '===Configuring connection to organization and Team Project==='
 az devops configure --defaults organization=$Organization project=$Project
 $ListOfIterations = az boards iteration project list --depth 1 | ConvertFrom-Json
 
+# if the root folder exists, do not create it again.
 if ($ListOfIterations.children.name -contains $StartDate.Year)
 {
     Write-Host "$($StartDate.Year) path already exists and won't be created."
@@ -37,14 +33,14 @@ else {
     Write-Host 'Created Root path: '$CreateRootIteration.name
 }
 
-#     $StartDateIteration = $StartDate
-# For ($i=1; $i -le $NumberOfSprints; $i++) 
-# {
-#     $Sprint = '2023-iteration-' + $i
-#     $FinishDateIteration = $StartDateIteration.AddDays(13)
-#     $createIteration = az boards iteration project create --name $Sprint --path $RootPath --start-date $StartDateIteration --finish-date $FinishDateIteration --org $Organization --project $Project | ConvertFrom-Json
-#     $addIteration = az boards iteration team add --id $createIteration.Identifier --team $TeamName --org $Organization --project $Project | ConvertFrom-Json
-#     Write-Host $addIteration.name 'created on path'$addIteration.path
-#     $StartDateIteration = $FinishDateIteration.AddDays(1)
-# }
+$StartDateIteration = $StartDate
+For ($i=1; $i -le $NumberOfSprints; $i++) 
+{
+    $Sprint = '2023-iteration-' + $i
+    $FinishDateIteration = $StartDateIteration.AddDays(13)
+    $createIteration = az boards iteration project create --name $Sprint --path $RootPath --start-date $StartDateIteration --finish-date $FinishDateIteration --org $Organization --project $Project | ConvertFrom-Json
+    $addIteration = az boards iteration team add --id $createIteration.Identifier --team $TeamName --org $Organization --project $Project | ConvertFrom-Json
+    Write-Host $addIteration.name 'created on path'$addIteration.path
+    $StartDateIteration = $FinishDateIteration.AddDays(1)
+}
 az devops logout
